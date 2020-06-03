9f4d3fcc515b0a9e07e8220563cb6b3e936c6a6e
vijendraRaghuwanshi <65821477+vijendraRaghuwanshi@users.noreply.github.com>
2020-06-03 01:06:04 -0500

    submit

---
+++ app/src/main/java/com/example/nusocial/ProfileActivity.java
@@ -0,0 +1,64 @@
+package com.example.nusocial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    public static final String GOOGLE_ACCOUNT = "google_account";
+    private TextView profileName, profileEmail;
+    private ImageView profileImage;
+    private Button signOut;
+    private GoogleSignInClient googleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        profileName = findViewById(R.id.profile_text);
+        profileEmail = findViewById(R.id.profile_email);
+        profileImage = findViewById(R.id.profile_image);
+        signOut = findViewById(R.id.sign_out);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*
+              Sign-out is initiated by simply calling the googleSignInClient.signOut API. We add a
+              listener which will be invoked once the sign out is the successful
+               */
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //On Succesful signout we navigate the user back to LoginActivity
+                        Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+
+        private void setDataOnView() {
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+        Picasso.with(this).load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
+        profileName.setText(googleSignInAccount.getDisplayName());
+        profileEmail.setText(googleSignInAccount.getEmail());
+    }
+
+}
